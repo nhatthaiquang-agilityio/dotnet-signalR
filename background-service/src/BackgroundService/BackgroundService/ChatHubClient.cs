8925cae894771d795d056443aa10bdcd457fb4de
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2019-05-22 14:26:22 +0700

    Receive message chat from dotnet app

---
+++ background-service/src/BackgroundService/BackgroundService/ChatHubClient.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace BackgroundService
+{
+    public class ChatHubClient : IHostedService
+    {
+        private readonly ILogger<ChatHubClient> _logger;
+        private HubConnection _connection;
+
+        public ChatHubClient(ILogger<ChatHubClient> logger)
+        {
+            _logger = logger;
+
+            _connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5000/chat")
+                .Build();
+
+            _connection.On<string, string>("Send", (nick, message) => Console.WriteLine(message));
+        }
+
+#region StartAsync
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            // Loop is here to wait until the server is running
+            while (true)
+            {
+                try
+                {
+                    await _connection.StartAsync(cancellationToken);
+                    break;
+                }
+                catch
+                {
+                    await Task.Delay(1000);
+                }
+            }
+        }
+#endregion
+#region StopAsync
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return _connection.DisposeAsync();
+        }
+    }
+#endregion
+}
---
+++ background-service/src/BackgroundService/BackgroundService/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace BackgroundService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddConsole();
+                })
+                .ConfigureServices((services) =>
+                {
+                    services.AddHostedService<ChatHubClient>();
+                })
+                .Build();
+
+            host.Run();
+
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+
+    }
+}
